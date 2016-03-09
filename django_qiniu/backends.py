c3e81e7ce3372fd2504030ae98078583b58da96f
Breeze.kay <wangwenpei@nextoa.com>
2016-03-09 16:44:46 +0800

    improve expire rule

--- django_qiniu/backends.py
+++ django_qiniu/backends.py
@@ -3,20 +3,14 @@
 from qiniustorage import backends
 from django.utils.encoding import filepath_to_uri
 from six.moves.urllib_parse import urljoin
-
+from django.conf import settings
 
 class QiniuStorage(backends.QiniuStorage):
     def url(self, name):
         name = self._normalize_name(self._clean_name(name))
         name = filepath_to_uri(name)
 
-        expire_str = name.split('/', 1)
-
-        try:
-            expire = int(expire_str[0])
-        except (TypeError, ValueError):
-            expire = 3600
-            pass
+        expire = 3600 if not hasattr(settings, 'QINIU_PREVIEW_EXPIRE') else settings.QINIU_PREVIEW_EXPIRE
 
         protocol = 'https://' if self.secure_url else 'http://'
         url = urljoin(protocol + self.bucket_domain, name)
